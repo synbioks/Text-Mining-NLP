bd0c58a376c6be3e0fc84ab5238f42e9c2d030c4
Xiang Lu <xil621@ucsd.edu>
2020-12-02 16:19:50 +0000

    Upload New File

---
+++ top-model/BIOBERT-hidden-CRF/src/Data/data_utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+
+ENTITY = "Chemicals"
+DATASETS = [x for x in os.listdir("/sbksvol/xiang/sbks_gitlab/top-model/BIOBERT/"+ENTITY)]
+if(".DS_Store" in DATASETS):
+    DATASETS.remove(".DS_Store")
+if(".ipynb_checkpoints" in DATASETS):
+    DATASETS.remove(".ipynb_checkpoints")
+
+EMBD_DIR = "/sbksvol/xiang/sbks_gitlab/top-model/BIOBERT/NER/data/interim/"+ENTITY
+SAVE_PATH = "/sbksvol/xiang/sbks_gitlab/top-model/BIOBERT/NER/data/processed/Chemical_512/"
+
+MAX_LEN = 512
+EMBEDDING = 768
+N_TAGS = 3
+CHUNK_SIZE = 16
+
+def chunks(lst, n):    
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+            
+count = 0       
+for dataset in DATASETs:
+    
+#     if dataset != "biosemantics":
+#         continue 
+    
+    embd = os.path.join(EMBD_DIR, dataset)
+
+    X_train = np.load(os.path.join(embd, "X_train.npy")) 
+    y_train = np.load(os.path.join(embd, "y_train.npy"))
+
+    X_train_gen = list(chunks(X_train, CHUNK_SIZE))
+    y_train_gen = list(chunks(y_train, CHUNK_SIZE))
+
+    for i,j in zip(X_train_gen, y_train_gen):
+        count += 1
+        with open(SAVE_PATH+'Train/'+dataset+'_{}.npy'.format(count), 'wb') as f:
+            np.save(f, i)
+        with open(SAVE_PATH+'Train_label/'+dataset+'_lab_{}.npy'.format(count), 'wb') as f:
+            np.save(f, j)
+        
