764474e29bcf9a2d999cec0edc0511b6be0b3a92
Jiawei Tang <jit072@ucsd.edu>
2021-05-20 16:26:50 -0700

    activation hook and lr schedule

--- relation-extraction/biobert_RE/models/pt/main.py
+++ relation-extraction/biobert_RE/models/pt/main.py
@@ -20,6 +20,40 @@ CKPT_ROOT = "../../weights"
 DATASET_ROOT = "../../datasets"
 CHEMPROT_ROOT = os.path.join(DATASET_ROOT, "CHEMPROT")
 
+# variable learning rate
+class NoamOptim:
+    "Optim wrapper that implements rate."
+    def __init__(self, model_size, init_step, warmup, factor, optimizer):
+        self.optimizer = optimizer
+        self._step = init_step
+        self.warmup = warmup
+        self.factor = factor
+        self.model_size = model_size
+        self._rate = 0
+        
+    def step(self):
+        "Update parameters and rate"
+        self._step += 1
+        rate = self.rate2()
+        for p in self.optimizer.param_groups:
+            p['lr'] = rate
+        self._rate = rate
+        self.optimizer.step()
+    
+    def zero_grad(self):
+        self.optimizer.zero_grad()
+        
+    def rate(self, step=None):
+        "Implement `lrate` above"
+        if step is None:
+            step = self._step
+        return self.factor * (self.model_size ** (-0.5) * min(step ** (-0.5), step * self.warmup ** (-1.5)))
+    
+    def rate2(self, step=None):
+        if step is None:
+            step = self._step
+        return self.factor * min(step ** (-0.5), step * self.warmup ** (-1.5))
+
 def train_net(task_name, net, train_dataloader, valid_dataloader, loss_fn, optimizer, max_step, valid_freq, ckpt_dir, init_step):
 
     # setup training
@@ -44,6 +78,7 @@ def train_net(task_name, net, train_dataloader, valid_dataloader, loss_fn, optim
 
             if train_step_count % valid_freq == 0:
                 net.eval()
+                print(f"Step {init_step + train_step_count} finished")
                 test_net("TRAIN", net, train_dataloader)
                 test_net("VALIDATION", net, valid_dataloader)
                 if ckpt_dir is not None:
@@ -64,7 +99,7 @@ def test_net(task_name, net, test_dataloader):
     num_classes = 4
     confusion_mat = np.zeros((num_classes, num_classes))
     with torch.no_grad():
-        for i, (x_batch, y_batch) in enumerate(test_dataloader):
+        for i, (x_batch, y_batch) in tqdm(enumerate(test_dataloader)):
 
             pred = net.predict(x_batch).cpu().numpy()
 
@@ -75,9 +110,23 @@ def test_net(task_name, net, test_dataloader):
             for p, y in zip(pred, y_batch):
                 confusion_mat[p][y] += 1
 
+    recalls = []
+    precisions = []
+    f1s = []
+    for i in range(num_classes):
+        recall = confusion_mat[i][i] / np.sum(confusion_mat[:, i])
+        precision = confusion_mat[i][i] / np.sum(confusion_mat[i])
+        recalls.append(recall)
+        precisions.append(precision)
+        f1s.append(2 * recall * precision / (recall + precision))
+
     print("---Results---")
     print("Total tested:", np.sum(confusion_mat))
     print("ACC:", num_correct / num_tested)
+    print("Precision:", precisions)
+    print("Recall:", recalls)
+    print("F1 Score", f1s)
+    print("F1 three cls", 2 * np.mean(precisions[:3]) * np.mean(recalls[:3]) / (np.mean(precisions[:3]) + np.mean(recalls[:3])))
     print(confusion_mat)
 
 def predict_net(task_name, net, dataloader):
@@ -98,7 +147,7 @@ def train_cls_end_to_end():
 
     init_state = None
     top_model_init_state = None
-    # init_state = "../../weights/chemprot-cls-end-to-end/5000"
+    # init_state = "../../weights/chemprot-cls-end-to-end/top-first-9000"
     # top_model_init_state = "../../weights/chemprot-cls-top-model/10000"
 
     # tokenizer
@@ -129,10 +178,20 @@ def train_cls_end_to_end():
         shuffle=False,
         collate_fn=chemprot_tsv_collate_fn
     )
+    
+    test_data = ChemprotTsvDataset(tokenizer, label_filter, max_seq_len)
+    test_data.load(os.path.join(CHEMPROT_ROOT, "test_4cls.tsv"))
+    test_dataloader = DataLoader(
+        dataset=test_data,
+        batch_size=256,
+        num_workers=8,
+        shuffle=False,
+        collate_fn=chemprot_tsv_collate_fn
+    )
 
     # net
     init_step = 0
-    net = CLSEndToEnd("../../weights/biobert-pt-v1.0-pubmed-pmc")
+    net = EndToEnd("../../weights/biobert-pt-v1.0-pubmed-pmc", top_model=CLSTopModel())
     if init_state is not None:
         state_filename = init_state.split("/")[-1]
         if "-" in state_filename:
@@ -144,7 +203,10 @@ def train_cls_end_to_end():
         net.top_model.load_state_dict(torch.load(top_model_init_state))
     net = net.cuda()
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(net.parameters(), lr=1e-5, betas=(0.9, 0.999), eps=1e-6, weight_decay=0.01)
+    adam = optim.AdamW(net.parameters(), lr=0, betas=(0.9, 0.999), eps=1e-6, weight_decay=0.01)
+    optimizer = NoamOptim(768, init_step, 3000, 0.0005, adam) 
+    # current best 768, init_step, 3000, 0.0005, adam, rate2() func
+    # current best 768, init_step, 3000, 0.02, adam, rate() func
 
     # train / test loop
     train_net(
@@ -154,15 +216,75 @@ def train_cls_end_to_end():
         valid_dataloader, 
         loss_fn, 
         optimizer, 
-        10000,  # max step
+        15000,  # max step
         1000,   # vaild frequency
         "../../weights/chemprot-cls-end-to-end", # checkpoint directory
         init_step
     ) 
 
+    test_net("TEST", net, test_dataloader)
+
+
+class activation_hook:
+
+    def __init__(self):
+        self.activations = []
+
+    def __call__(self, net, x, y):
+        self.activations.append(y.detach().cpu().numpy())
+
+    def get_act(self):
+        res = []
+        for batch in self.activations:
+            for item in batch:
+                res.append(item)
+        return np.array(res)
+
+def gen_chemprot_act(model_name):
+
+    init_state = f"../../weights/chemprot-cls-end-to-end/{model_name}"
+    
+    tokenizer = BertTokenizer(
+        "../../weights/biobert-pt-v1.0-pubmed-pmc/vocab.txt",
+        do_lower_case=False
+    )
+    label_filter = ["CPR:3", "CPR:4", "CPR:9", "false"]
+    max_seq_len = 128
+
+    test_data = ChemprotTsvDataset(tokenizer, label_filter, max_seq_len)
+    test_data.load(os.path.join(CHEMPROT_ROOT, "test_4cls.tsv"))
+    test_dataloader = DataLoader(
+        dataset=test_data,
+        batch_size=256,
+        num_workers=8,
+        shuffle=False,
+        collate_fn=chemprot_tsv_collate_fn
+    )
+
+    net = EndToEnd("../../weights/biobert-pt-v1.0-pubmed-pmc", top_model=CLSTopModel())
+    net.load_state_dict(torch.load(init_state))
+    net = net.cuda().eval()
+    act0 = activation_hook()
+    act1 = activation_hook()
+    act2 = activation_hook()
+    net.top_model.fc[1].register_forward_hook(act0)
+    net.top_model.fc[3].register_forward_hook(act1)
+    net.top_model.fc[5].register_forward_hook(act2)
+    test_net("TEST", net, test_dataloader)
+    
+    with open(f"activations/{model_name}/l0.npy", "wb") as fout:
+        np.save(fout, act0.get_act())
+
+    with open(f"activations/{model_name}/l1.npy", "wb") as fout:
+        np.save(fout, act1.get_act())
+
+    with open(f"activations/{model_name}/l2.npy", "wb") as fout:
+        np.save(fout, act2.get_act())
+
+
 def acs_predict():
 
-    dataset_dir = "../../datasets/acs-20210331-gold"
+    dataset_dir = "../../datasets/acs-20210505-kevin"
 
     # tokenizer
     tokenizer = BertTokenizer(
@@ -170,7 +292,7 @@ def acs_predict():
         do_lower_case=False
     )
 
-    net = CLSEndToEnd("../../weights/biobert-pt-v1.0-pubmed-pmc")
+    net = EndToEnd("../../weights/biobert-pt-v1.0-pubmed-pmc")
     net.load_state_dict(torch.load("../../weights/chemprot-cls-end-to-end/top-first-9000"))
     net = net.cuda()
     net.eval()
@@ -255,6 +377,7 @@ if __name__ == "__main__":
     # 5e-5 to 1e-5 early stopping
     # top layer pooling
 
-    acs_predict()
+    # acs_predict()
     # train_cls_end_to_end()
     # train_cls_top_model()
+    gen_chemprot_act("3layer-e2e-narrow-relu")
