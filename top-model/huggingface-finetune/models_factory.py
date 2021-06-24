645a80d7a46f86e10c54dff0e59f3fa08e31f706
nikhilkarnwal <nikhilkarnwal@yahoo.com>
2021-06-24 16:07:56 -0700

    bug fix

--- top-model/huggingface-finetune/models_factory.py
+++ top-model/huggingface-finetune/models_factory.py
@@ -1,6 +1,6 @@
-from .models.models_enum import ModelsType
-from .models.models import BertNERTopModel
-from .models.models_fcn import BertTopModelE2E
+from models.models_enum import ModelsType
+from models.models import BertNERTopModel
+from models.models_fcn import BertTopModelE2E
 from transformers import BertForTokenClassification
 
 
