943ab3783ba7447bbe386320f72b55be69e68a0c
James Bentley <jbentley2@vgr-1-20.cm.cluster>
2024-01-24 11:03:46 -0800

    removed relation-extraction from parent directory, moved to voyager directory

--- relation-extraction/biobert_RE/run_multicard.sh
+++
@@ -1,9 +0,0 @@
-export PYTHONPATH=/home/jbentley2/installs/v1.9/tf/local/lib/python3.10/dist-packages:/usr/lib/habanalabs:$PYTHONPATH
-export PHY_CPU_COUNT=$(lscpu --all --parse=CORE,SOCKET | grep -Ev "^#" | sort -u | wc -l)
-export PHY_HPU_COUNT=$(ls /dev/hl? | wc -l)
-export MPI_PE=$(($PHY_CPU_COUNT/$PHY_HPU_COUNT))
-
-export TRANSFORMERS_CACHE=/home/jbentley2/relation-extraction/cache
-export HABANA_VISIBLE_MODULES=0,1,2,3,4,5,6,7
-
-mpirun --allow-run-as-root -np 8 python3 -m models.train_multicard >& multicard_train.log
--- relation-extraction/biobert_RE/run_single.sh
+++
@@ -1,6 +0,0 @@
-export PYTHONPATH=/home/jbentley2/installs/v1.9/tf/local/lib/python3.10/dist-packages:/usr/lib/habanalabs:$PYTHONPATH
-export TRANSFORMERS_CACHE=/home/jbentley2/relation-extraction/cache
-
-python3 -m models.train >& single_train_2.log
-
-# tail train.log
